25e92066f681a0720799aad06e18787d50bc75e6
hnmx4 <hnmnty@gmail.com>
2021-03-25 14:17:55 +0900

    use jQuery for side-section items

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
-$("h3").before("<span class='bullet'></span>");
+const genInfoSectionItem = (name, text, labelClass) => {
+    const item = `
+<li class="item">
+    <div class="item-label"><i class="${labelClass}"></i></div>
+    <div class="item-text-wrap">
+        <div class="item-name">${name}</div>
+        <div class="item-text">${text}</div>
+    </div>
+</li>
+`;
+    return(item);
+};
 
-$(function() {
-    });
+const genSocialSectionItem = (name, text, url, labelClass) => {
+    const item = `
+<li class="item">
+    <div class="item-label"><i class="${labelClass}"></i></div>
+    <div class="item-text-wrap">
+        <div class="item-name">${name}</div>
+        <div class="item-text"><a href="${url}">${text}</a></div>
+    </div>
+</li>
+`;
+    return(item);
+};
